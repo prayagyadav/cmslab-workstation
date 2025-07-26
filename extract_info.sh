7de08ceb370abdcd11698be84ea54a15c5e44831
prayagyadav <prayagyadav206@gmail.com>
2025-07-26 10:08:50 +0200

    Last Updated: Sat Jul 26 10:08:50 AM CEST 2025

--- extract_info.sh
+++ extract_info.sh
@@ -2,7 +2,8 @@
 
 
 # IP Address
-ip_address="$(ip address | grep eth0 | grep inet | awk '{print $2}' | cut -d '/' -f 1)"
+#ip_address="$(ip address | grep eth0 | grep inet | awk '{print $2}' | cut -d '/' -f 1)"
+ip_address=$(ip -o -4 addr show dev eth0 | awk '{print $4}' | cut -d/ -f1)
 echo "IP: ${ip_address}"
 
 
@@ -10,5 +11,5 @@ echo "IP: ${ip_address}"
 
 
 # Update the json
-jq '.last_updated = "$(date)" | .ipaddress = "${ip_address}"' dashboard.json > tmp.json && mv tmp.json dashboard.json
+jq ".last_updated = "$(date)" | .ipaddress = "${ip_address}"" dashboard.json > tmp.json && mv tmp.json dashboard.json
 
